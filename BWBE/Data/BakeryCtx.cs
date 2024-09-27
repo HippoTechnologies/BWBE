0794198f81676c5d0997c5b0a3b1b7d7bdb2b73a
chromody <chromody@gmail.com>
2024-09-27 18:21:47 -0500

    forgot to update everything else

--- BWBE/Data/BakeryCtx.cs
+++ BWBE/Data/BakeryCtx.cs
@@ -26,8 +26,16 @@ public class BakeryCtx : DbContext
     public DbSet<PhoneNumber> PhoneNumber => Set<PhoneNumber>();
     
     public DbSet<InventoryItem> InventoryItem => Set<InventoryItem>();
+
+    public DbSet<Ingredient> Ingredient => Set<Ingredient>();
     
     public DbSet<Recipe> Recipe => Set<Recipe>();
     
     public DbSet<CookStep>CookStep => Set<CookStep>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ingredient>()
+            .HasKey(e => new { e.Id, e.RecipeId }); // Define composite key
+    }
 }
