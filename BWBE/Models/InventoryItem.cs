b0f47f41459c6f76cd5d72c8fdcfd64c2bda51fd
chromody <chromody@gmail.com>
2024-10-09 23:15:24 -0500

    changed quantity from inventory to work with floats

--- BWBE/Models/InventoryItem.cs
+++ BWBE/Models/InventoryItem.cs
@@ -6,7 +6,7 @@ public class InventoryItem
 {
     [MaxLength(36)] public string Id { get; set; } = null!;
     [MaxLength(50)] public string Name { get; set; } = null!;
-    public int Quantity { get; set; }
+    public float Quantity { get; set; }
     public int PurchaseQuantity { get; set; }
     public float CostPerPurchaseUnit { get; set; }
     [MaxLength(50)] public string Unit { get; set; } = null!;
